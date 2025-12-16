56dd065da21dc02f400c4b1adcbc708b4ea719b6
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-16 10:13:58 +0530

    configure dotenv

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./index.js");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port", process.env.PORT);
 });
