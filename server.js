ca7c55d8688ae6bfb225cefe35ee525b0f049919
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-16 11:59:46 +0530

    connecting db

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app = require("./index.js");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config({ path: "./config.env" });
+
+mongoose
+.connect(process.env.DB_URL)
+.then(()=>{
+    console.log("Connected to database");
+})
+.catch((err)=>{
+    console.log("Error connecting to database", err);
+});
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port", process.env.PORT);
