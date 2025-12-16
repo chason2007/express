5a761525bf05c142a95cad0aebdb302c88f328a0
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-16 10:02:27 +0530

    server.js file created

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ const studentRoutes = require("./routes/studentRoutes");
 app.use(express.json());
 app.use("/api/v3/students", studentRoutes);
 
-app.listen(3000,() =>{
-    console.log("Server started on port 3000");
-})
+module.exports = app;
